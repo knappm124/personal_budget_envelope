f43b10d1b058c9d53643abe56f1e04e40b21b707
Melissa Knapp <knappm124@gmail.com>
2023-01-10 13:43:22 -0500

    created dummy envelopes

---
+++ data.js
@@ -0,0 +1,22 @@
+export const envelopes = [
+    {
+        id: 1,
+        name: 'Utilities',
+        amount: '350'
+    },
+    {
+        id: 2,
+        name: 'Rent',
+        amount: '2800'
+    },
+    {
+        id: 3,
+        name: 'Groceries',
+        amount: '500'
+    },
+    {
+        id: 4,
+        name: 'Bills',
+        amount: 1500
+    }
+];
