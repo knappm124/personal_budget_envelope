c74e81f0245430c7cd00d689359933268b03f9f8
Melissa Knapp <knappm124@gmail.com>
2023-01-10 16:59:20 -0500

    made helper functions in utils and fixed data type

--- data.js
+++ data.js
@@ -1,22 +1,29 @@
-export const envelopes = [
-    {
-        id: 1,
-        name: 'Utilities',
-        amount: '350'
-    },
-    {
-        id: 2,
-        name: 'Rent',
-        amount: '2800'
-    },
-    {
-        id: 3,
-        name: 'Groceries',
-        amount: '500'
-    },
-    {
-        id: 4,
-        name: 'Bills',
-        amount: 1500
-    }
-];
+const envelopes = {
+    nextId: 5,
+    array: [
+        {
+            id: 1,
+            name: 'Utilities',
+            amount: 350
+        },
+        {
+            id: 2,
+            name: 'Rent',
+            amount: 2800
+        },
+        {
+            id: 3,
+            name: 'Groceries',
+            amount: 500
+        },
+        {
+            id: 4,
+            name: 'Bills',
+            amount: 1500
+        }
+    ]
+};
+
+module.exports = {
+    envelopes
+}
---
+++ utils.js
@@ -0,0 +1,47 @@
+const { envelopes } = require('./data');
+
+const findEnvelope = id => {
+    for(let i = 0; i < envelopes.array.length; i++){
+        if(envelopes.array[i].id == id){
+            return i;
+        }
+    }
+    return -1;
+}
+
+const updateEnvelopes = (id, newEnvelope) => {
+    if(typeof newEnvelope.name != 'string' || newEnvelope.amount <= 0) {
+        return Error('Error: Updated envelope must have a valid name and amount')
+    }
+    let index = findEnvelope(id);
+    if(index == -1){
+        return Error('Error: Id not found');
+    }
+    newEnvelope.id = id;
+    envelopes.array[index] = newEnvelope;
+    return newEnvelope;
+}
+
+const addEnvelope = newEnvelope => {
+    if(typeof newEnvelope.name != 'string' || newEnvelope.amount <= 0) {
+        return Error('Error: New envelope must have a valid name and amount');
+    }
+    newEnvelope.id = envelopes.nextId;
+    envelopes.array.push(newEnvelope);
+    envelopes.nextId += 1;
+    return envelopes.array;
+}
+
+const deleteEnvelope = id => {
+    let index = findEnvelope(id);
+    let temp = envelopes.array[index];
+    if(index == -1){
+        return Error('Error: Id not found');
+    }
+    envelopes.array.splice(index, 1);
+    return temp;
+}
+
+module.exports = {
+    updateEnvelopes, addEnvelope, deleteEnvelope
+};
